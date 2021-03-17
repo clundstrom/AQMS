95d6c69b6e243c6c9f5a6f4d120ede52ac5fa1d7
Camilla Heiding <ch223cd@student.lnu.se>
2021-03-17 13:31:12 +0100

    Added copy of updated decoder in TTN

--- MKR1300/decoder_TTN.js
+++ MKR1300/decoder_TTN.js
@@ -2,12 +2,21 @@ function Decoder(bytes, port) {
   // Decode an uplink message from a buffer
   // (array) of bytes to an object of fields.
   
-  var t = (bytes[0] & 0x80 ? 0xFFFF<<16 : 0) | bytes[0]<<8 | bytes[1];
-  var h = bytes[2];
-  
-  return {
-    temperature: t / 10,
-    humidity: h
-  }
+  var t;
+  var h;
+  var p;
+  var d;
 
+    t = (bytes[0] & 0x80 ? 0xFFFF<<16 : 0) | bytes[0]<<8 | bytes[1];
+    h = bytes[2];
+    p = bytes[3] << 16 | bytes[4] << 8 | bytes[5];
+    d = bytes[6] << 8 | bytes[7]; 
+  
+    return {
+      temperature: t / 10,
+      humidity: h,
+      pressure: p/100,
+      dustDensity: d
+    }
+    
 }
